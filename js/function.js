5797281ef21a48a99a5d6892663d51d33d4db60e
luiz gustavo <amaralluis176@gmail.com>
2026-04-09 22:56:01 -0300

    os estilos ainda não foram alterados

---
+++ js/function.js
@@ -0,0 +1,119 @@
+//vamos animar a parte do do serviços
+function animateServicos(elemento){
+
+
+
+
+  const callModal = document.querySelector(".popup")
+  const clickServ = document.querySelectorAll("#img");
+  const exit = document.querySelector("#exit-btn");
+  const popupImg = document.querySelector("#img-popup")
+  
+   //pegar o src da imagem
+   const valElem = elemento.getAttribute('src');
+   console.log(valElem);
+
+   popupImg.setAttribute( 'src' ,valElem);
+
+  clickServ.forEach((image) =>{
+    image.addEventListener('click',function(){
+      callModal.style.display = "flex";  //continuar a dar imagem ao pop-up
+    })
+  })
+
+  exit.addEventListener('click', ()=>{
+
+    callModal.style.display = "none";
+    
+})
+
+
+  
+   
+  
+
+
+}
+
+
+function animateheader(){
+    const animar = document.querySelectorAll(".off");
+    
+
+        animar.forEach((animation , index) =>{
+          setTimeout(() => {
+             animation.classList.add("active");
+          }, index * 500); // 200ms de delay entre cada item
+        })
+        
+
+        
+
+    
+
+}
+
+const controls = document.querySelectorAll('.control');
+const items = document.querySelectorAll('.item');
+
+let currentItem = 0;
+const maxItems = items.length;
+
+// função que atualiza o slider
+function updateSlider() {
+    items.forEach((item, index) => {
+        item.classList.remove("current-item", "prev", "next", "hidden");
+
+        if (index === currentItem) {
+            item.classList.add("current-item");
+        } 
+        else if (index === (currentItem - 1 + maxItems) % maxItems) {
+            item.classList.add("prev");
+        } 
+        else if (index === (currentItem + 1) % maxItems) {
+            item.classList.add("next");
+        } 
+        else {
+            item.classList.add("hidden");
+        }
+    });
+}
+
+// eventos dos botões
+controls.forEach(control => {
+    control.addEventListener('click', (event) => {
+
+        const direction = event.currentTarget.id;
+
+        if (direction === "left") {
+            currentItem--;
+        } else {
+            currentItem++;
+        }
+
+        // loop infinito
+        if (currentItem >= maxItems) currentItem = 0;
+        if (currentItem < 0) currentItem = maxItems - 1;
+
+        updateSlider();
+    });
+});
+
+const elements = document.querySelectorAll('.hide');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+elements.forEach((el) => observer.observe(el));
+
+updateSlider();
+
+
+
+
+animateheader();
